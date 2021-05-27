f0e92cfd06fd7feab07cd303c9961c134c86758e
Tadhg Lewis <tadhg.lewis@outlook.com>
2021-05-27 22:15:15 +1000

    fix build public path config

--- sku.config.js
+++ sku.config.js
@@ -2,6 +2,6 @@ module.exports = {
   clientEntry: 'src/client.tsx',
   renderEntry: 'src/render.tsx',
   sites: [{ name: 'apac', host: 'dev.apac.com' }],
-  publicPath: '/path/to/public/assets/', // <-- Required for sku build output
+  publicPath: 'dist', // <-- Required for sku build output
   orderImports: true,
 };
